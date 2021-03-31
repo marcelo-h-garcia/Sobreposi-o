1ceb972ba59e84e5d209a1c57743bbe264503ccf
Marcelo Helio Garcia <marceloph2@yahoo.com.br>
2021-03-31 11:27:26 -0300

    Sobreposição, palavra super, anotação @Override

---
+++ src/entities/BusinesAccount.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class BusinesAccount extends Account {
+	private Double loanLimit;
+	
+	public BusinesAccount() {
+		super();
+	}
+
+	public BusinesAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan(double amount) {
+		if(amount <= loanLimit) {
+		balance += amount - 10.0;
+		}
+	}
+	
+	@Override
+	public void withdraw(double amount) {
+		super.withdraw(amount);
+		balance -= 2.0;
+	}
+}
